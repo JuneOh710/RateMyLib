3b46c152ecb8c898220b42882cf51d2841c20863
June Oh <JuneOh710@gmail.com>
2021-06-21 17:30:18 -0400

    got the images to load properly

--- app.js
+++ app.js
@@ -23,13 +23,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+
+// I don't know what I'm doing here.
+// const options = { index: false, redirect: false }
+
 app.use(express.static('seeds'));
+
 
 // library routes
 // get home page
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
+
+
 // get libraries index page
 app.get('/libraries', async (req, res) => {
     const libraries = await Library.find({})
@@ -42,6 +49,20 @@ app.get('/libraries/:libId', async (req, res) => {
     res.render('libraries/show.ejs', { library: l })
 })
 
+
+
+// study spot routes
+// get all study spots 
+app.get('/studySpots', async (req, res) => {
+    const studySpots = await StudySpot.find({}).populate('library', 'name')
+    res.render('studySpots/index.ejs', { studySpots })
+})
+// get details of study spot
+app.get('/studySpots/:id', async (req, res) => {
+    const { id } = req.params;
+    const studySpot = await StudySpot.findById(id).populate('library', 'name')
+    res.render('studySpots/show.ejs', { studySpot })
+})
 
 app.listen(4000, () => {
     console.log('Serving on port 4000')
